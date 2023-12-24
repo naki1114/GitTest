058e004ea9e4ea2aa982ffcdfbffc32fb14c4198
Oscar <rkdgjdl@naver.com>
2023-12-24 23:55:47 +0900

    oscar 666

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         // branch test2
         // asdfsafdsfsdfdsfsafsfsfesfesfsfaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
         // oscaroscaroscaroscaroscaroscaroscaroscaroscaroscaroscaroscaroscaroscar
+        // oscaroscaroscaroscaroscaroscaroscaroscaroscaroscaroscaroscaroscaroscar
+        // oscaroscaroscaroscaroscaroscaroscaroscaroscaroscaroscaroscaroscaroscar
 
     }
 
