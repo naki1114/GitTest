c5a8d7f34f52d83415a3c35750cbfd81fe4403a8
Naki <naki1114@naver.com>
2023-12-25 02:44:26 +0900

    Git Test : Naki Test
    Commit Test

---
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.gittest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var tv : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tv = findViewById(R.id.tv)
+    }
+
+}
