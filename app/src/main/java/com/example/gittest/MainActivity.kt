ff2718d198ea44f1ad321f9b2392dd4940c7bdd7
Naki <naki1114@naver.com>
2023-12-24 22:13:03 +0900

    branch test1

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.gittest
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -15,31 +14,8 @@ class MainActivity : AppCompatActivity() {
 
         tv = findViewById(R.id.tv)
 
-        nakiTest4()
+        // branch test1
 
-        // 대충 주석 추가
-
-        // naki test3
-        // 테스트테스트테스트테스트테스트
-        //테스트테스트테스트테스트테스트테스트
-
-        nakiTest7()
-
-    }
-
-    // naki test4
-    fun nakiTest4() {
-        tv.text = "Naki4"
-    }
-
-    // naki test5
-    fun nakiTest5() {
-        tv.text = "Naki5"
-    }
-
-    // naki test7
-    fun nakiTest7() {
-        tv.text = "Naki7"
     }
 
 }
