4f4631d2329689bfaf696860d3bba21df9576974
Oscar <rkdgjdl@naver.com>
2023-12-24 23:40:50 +0900

    oscar test

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         // branch test1
         // branch test2
         // asdfsafdsfsdfdsfsafsfsfesfesfsfaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        // oscaroscaroscaroscaroscaroscaroscar
 
     }
 
